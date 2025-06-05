fb7d9320937c9f7c534001217b045c3352937292
joaovitorpiccolo <joaovitorpiccolo015@gmail.com>
2025-06-05 12:34:45 -0300

    starting database structure

--- src/gs_backend/settings.py
+++ src/gs_backend/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 class Settings(BaseSettings):
     api_key: str
+    DATABASE_URL: str
     model_config = SettingsConfigDict(
         env_file=Path(__file__).resolve().parents[2] / '.env',
-        env_file_encoding='utf-8'
-    )
+        env_file_encoding='utf-8')

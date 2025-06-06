891868a124f72b74c959acfd3007e32170edba16
joaovitorpiccolo <joaovitorpiccolo015@gmail.com>
2025-06-05 22:24:26 -0300

    removing path

--- src/gs_backend/settings.py
+++ src/gs_backend/settings.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
 
 
 class Settings(BaseSettings):
@@ -6,6 +7,6 @@ class Settings(BaseSettings):
     api_key: str
     DATABASE_URL: str
     model_config = SettingsConfigDict(
-        env_file=str('C:\\Users\\janj\\PycharmProjects\\gs_backend\\.env'),
+        env_file=Path(__file__).resolve().parents[2] / '.env',
         env_file_encoding='utf-8'
     )
